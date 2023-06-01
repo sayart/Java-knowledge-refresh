586d8c26d2e4e507f6aed659d03420afa9f3316d
Artem Slobodianiuk <sloboa01@kits.kfplc.com>
2023-06-01 11:07:43 +0200

    task 1 Create a Class and Object

---
+++ Task1OOP/src/com/gmail/slartua/task1/Car.java
@@ -0,0 +1,25 @@
+package com.gmail.slartua.task1;
+
+public class Car {
+    private String make;
+    private String model;
+    private int year;
+    private double mileage;
+
+    public Car(String make, String model, int year, double mileage) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", mileage=" + mileage +
+                '}';
+    }
+}
---
+++ Task1OOP/src/com/gmail/slartua/task1/Main.java
@@ -0,0 +1,10 @@
+package com.gmail.slartua.task1;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Nissan", "Note", 2007, 230000.0);
+        System.out.println(car);
+    }
+}
